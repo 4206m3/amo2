f6d0bb616f3e838e7da766f434715f9fd067730c
evgemih <118483471+evgemih@users.noreply.github.com>
2023-06-13 17:29:37 +0300

    Добавил комменты

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "\n========== Create dataset =========="
 python3 data_creation.py
+echo "\n======== Model preparation ========="
 python3 model_preparation.py
+echo "\n======= Model preprocessing ========"
 python3 model_preprocessing.py
+echo "\n========== Model testing ==========="
 python3 model_testing.py
