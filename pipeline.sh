1699deb3eec588a4fdca0d1aa3c2d3fd78de40b9
evgemih <118483471+evgemih@users.noreply.github.com>
2023-06-13 17:34:09 +0300

    Исправил порядок следования

--- pipeline.sh
+++ pipeline.sh
@@ -2,9 +2,9 @@
 
 echo "\n========== Create dataset =========="
 python3 data_creation.py
-echo "\n======== Model preparation ========="
-python3 model_preparation.py
 echo "\n======= Model preprocessing ========"
 python3 model_preprocessing.py
+echo "\n======== Model preparation ========="
+python3 model_preparation.py
 echo "\n========== Model testing ==========="
 python3 model_testing.py
